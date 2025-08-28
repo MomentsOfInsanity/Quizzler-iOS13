95d40eb432484a809d23107cba1029a3daf32da2
Raj Bedi <71458567+MomentsOfInsanity@users.noreply.github.com>
2025-08-28 18:00:38 -0400

    Move and update QuizBrain, add score tracking
    QuizBrain.swift was moved from Controller to Model and updated to include score tracking and improved method signatures. ViewController was updated to display the score and use the new QuizBrain API. Minor cleanups were made to Question.swift and project files to reflect these changes.

--- Quizzler-iOS13/Model/Question.swift
+++ Quizzler-iOS13/Model/Question.swift
@@ -2,20 +2,20 @@
 //  Question.swift
 //  Quizzler-iOS13
 //
-//  Created by Raj Bedi on 8/11/25.
-//  Copyright © 2025 The App Brewery. All rights reserved.
+//  Created by Angela Yu on 07/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import Foundation
 
 struct Question {
-    
     let text: String
     let answer: String
     
-    init(q: String, a: String){
+    init(q: String, a: String) {
         text = q
         answer = a
     }
-    
 }
+
+    
